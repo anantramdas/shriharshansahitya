3a6185b5574f8e146e9dac4199a4b13cddb02bf1
Anantram Das <anantramdas20@gmail.com>
2021-06-08 01:39:32 +0530

    first commit

---
+++ booklist.js
@@ -0,0 +1,207 @@
+var booksList = {
+    'gita_gyaan': {
+        pageCount: 285,
+        title: 'गीता ज्ञान',
+        intro: `इस टीका के अनुशीलन से गीता के सभी रहस्यार्थों का बोध हो जाता है। यह टीका, विषय क्रम भंग, अनुवृत्तिबाहुल्य एवं विस्ताराडम्बर आदि दोषों से सर्वथा शून्य है।`,
+        introAuthor: '— डॉ. रामकिंकर दास',
+    },
+    'leela_sudha_sindhu': {
+        pageCount: 745,
+        title: 'लीला सुधा सिन्धु',
+        intro: `"लीला-सुधा-सिन्धु" वस्तुतः लीला सुधा का सागर ही नहीं अपितु महासागर है... रसिक-जन आत्मा तथा मानस के हेतु इसमें अनेक रागों से रंजित पदावलियों की षोडश-शत रत्नराशि सन्निहित है, जिसका एक-एक अमूल्य रत्न प्राप्त हो जाने पर उस लीलामय शाहंशाह... को खरीद लेने हेतु पर्याप्त है।`,
+        introAuthor: '— श्री हरिगोविन्द दास (द्विवेदी)',
+    },
+    'panch_shatak': {
+        pageCount: 29,
+        title: 'पंच शतक',
+        intro: `यह श्री स्वामी जी द्वारा रचित पाँच शतकों का संग्रह है जिसमें क्रमशः श्री सदगुरुदेव भगवान, श्री हनुमान जी, श्री संत भगवान, श्री सीता जी एवं श्री राम जी का दोहा चौपाई मे बड़ा ही सुंदर गुणगान किया गया है।`,
+        introAuthor: '',
+    },
+    'mithila_madhuri': {
+        pageCount: 73,
+        title: 'मिथिला माधुरी',
+        intro: `श्री स्वामी जी की यह कृति मैथिल रस की मधुरता की बड़ी ही मनोहारी छवि है।`,
+        introAuthor: '',
+    },
+    'ras_chandrika': {
+        pageCount: 43,
+        title: 'रस चंद्रिका',
+        intro: `वेद वर्णित पूर्णतम परब्रह्म रस स्वरूप है, रसो वै सः। उस अनन्त रस की प्राप्ति ही परम पुरुषार्थ है। प्रस्तुत पुस्तक रस चन्द्रिका में रस प्राप्ति के साधन पर भली भाँति प्रकाश डाला गया है।`,
+        introAuthor: '— पद-पंकज-भ्रमर<br>सीतारामदास',
+    },
+    'vaishnviya_vigyan': {
+        pageCount: 150,
+        title: 'वैष्णवीय विज्ञान',
+        intro: `वेद काल से लेकर वर्तमान काल तक अनेक ग्रंथ लिखे जा चुके हैं। दास ने वैष्णवीय-विज्ञान लिखने का चापल्य वैष्णव समाज की कमी की पूर्ति के लिए नहीं किया है, अपितु सिन्धु को एक अंजली जल... समर्पित करने के समान कैंकर्य किया...`,
+        introAuthor: '— ग्रंथ लेखक<br>श्री राम हर्षण दास',
+    },
+    'atma_ramayan': {
+        pageCount: 41,
+        title: 'आत्म रामायण',
+        intro: `प्रस्तुत रचना का हेतु अंतर्यामी प्रभु ही जानें। श्री स्वामी जी रचित 'गीता-ज्ञान' हो अथवा 'औपनिषद ब्रह्मबोध', 'प्रेमरामायण' महाप्रबन्ध हो अथवा 'वैष्णवीय विज्ञान' सभी गद्य ग्रन्थों में समान रूप से स्वामीपाद् के आत्मचरित्र का साक्षात्कार... होता रहा है जैसे मक्खन में घी का...`,
+        introAuthor: '— श्रीकृष्ण उपाध्याय',
+    },
+    'nootan_aarti_bhavanjali': {
+        pageCount: 78,
+        title: 'नूतन आरती भावांजलि',
+        intro: `आरती की प्रदीप्त-बत्तियों पर हाँथ घुमाकर सिर में रखने का अर्थ है, भगवान के कष्ट को अपने ऊपर धारण करना। इस प्रकार जो भगवान की आरती को अपने सिर पर धारण करता है, वह स्वयं सब प्रकार की आर्तियों से मुक्त हो जाता है।`,
+        introAuthor: '— श्री महान्त हरिदास जी',
+    },
+    'prapatti_darshan': {
+        pageCount: 224,
+        title: 'प्रपत्ति दर्शन',
+        intro: `प्राचीन ग्रन्थों में वैष्णवाचार्यों से वर्णित शरणागति धर्म का ज्ञान मुमुक्षुओ को सुलभ ही रहा है किन्तु इन ग्रन्थों की प्रपत्ति परक वार्तायें सहजतया सबके समझ में नहीं आती, ... इसलिए दास ने स्वाश्रित प्रपन्न भक्तों के हितार्थ स्वरचित सरल सूत्रों की, सरल व्याख्या लिखकर, ...`,
+        introAuthor: '— श्री राम हर्षण दास',
+    },
+    'prem_prabha': {
+        pageCount: 63,
+        title: 'प्रेम प्रभा',
+        intro: ``,
+        introAuthor: '',
+    },
+    'ras_vigyan': {
+        pageCount: 23,
+        title: 'रस विज्ञान',
+        intro: ``,
+        introAuthor: '',
+    },
+    'ram_rajya': {
+        pageCount: 55,
+        title: 'राम राज्य',
+        intro: ``,
+        introAuthor: '',
+    },
+    'leela_vilas': {
+        pageCount: 36,
+        title: 'लीला विलास',
+        intro: ``,
+        introAuthor: '',
+    },
+    'virah_vallari': {
+        pageCount: 96,
+        title: 'विरह वल्लरी',
+        intro: `विरह प्रियतम को आशु द्रवित करने का एक अचूक एवं सिद्ध साधन है। इस लघु कलेवर ग्रंथ में गागर में सागर भरने का सफल प्रयास किया गया है। आशा है आचार्य प्रवर की यह विरह-रस-गुटिका पाठकजनों को भवरोग से मुक्तकर, प्रियतम परमात्मा से मिलाने में अचूक सिद्ध होगी।`,
+        introAuthor: '— श्री हरिगोविन्द दास (द्विवेदी)',
+    },
+    'vishuddha_brhma_bodh': {
+        pageCount: 97,
+        title: 'विशुद्ध ब्रह्म-बोध',
+        intro: `विद्या, वास्तव में ब्रह्मविद्या ही है, जो साक्षात् सच्चिदानन्द स्वरूपिणी है... अस्तु वही ब्रह्मज्ञान जो परम्परागत सद्गुरुओं से सदशिष्यों को प्रदान किया जाता था और किया जा रहा है, इस ग्रंथ में अपनी बुद्धि के अनुसार गुरु-शिष्य के संवाद रूप में प्रस्तुत किया गया है।`,
+        introAuthor: '— श्री राम हर्षण दास',
+    },
+    'vaidehi_darshan': {
+        pageCount: 33,
+        title: 'वैदेही दर्शन',
+        intro: `प्रस्तुत ग्रन्थ में श्री आचार्य महाप्रभु ने श्री सीता तत्व का किञ्चित रहस्योद्घाटन किया है। गुरु शिष्य के संवाद के रूप में श्रीजी का अनन्त वैभव, उनका स्वरूप(आध्यात्मिक, अधिदैविक व अधिभौतिक) तथा उनकी कृपा परवशता का दर्शन कराया गया है।`,
+        introAuthor: '',
+    },
+    'shri_lakshminidhi_nikunj_ki_ashtayamiya_seva': {
+        pageCount: 99,
+        title: 'श्री लक्ष्मीनिधि निकुंज की अष्टयामीय सेवा',
+        intro: ``,
+        introAuthor: '',
+    },
+    'shri_sita_janma_prakash': {
+        pageCount: 24,
+        title: 'श्री सीता जन्म प्रकाश',
+        intro: ``,
+        introAuthor: '',
+    },
+    'shri_sitaram_vivahashtakam': {
+        pageCount: 4,
+        title: 'श्री सीताराम विवाहाष्टकम्',
+        intro: ``,
+        introAuthor: '',
+    },
+    'siddhi_sadan_ki_ashtayamiya_seva': {
+        pageCount: 33,
+        title: 'सिद्धि सदन की अष्टयामीय सेवा',
+        intro: ``,
+        introAuthor: '',
+    },
+    'harshan_satsai': {
+        pageCount: 102,
+        title: 'हर्षण सतसई',
+        intro: `प्रस्तुत ग्रंथ में श्री आचार्यपाद ने सात शतकों क्रमशः नाम, रूप, लीला, धाम, प्रपत्ति, प्रेम एवं प्रेमोपलम्भन-परत्व-प्रकरण के स्वरूपों में... जो प्रेम रस रूपी सरिता में अवगाहित पाठक वृन्दों को प्रेमाप्लावित करेगी...`,
+        introAuthor: '— श्री अवध किशोर दास',
+    },
+    'updeshamrit': {
+        pageCount: 123,
+        title: 'उपदेशामृत',
+        intro: `प्रस्तुत ग्रंथ में, आचार्य प्रभु से समय-समय में अपने आश्रितों एवं स्नेह भाजनों के नाम जो उपदेश, पत्रों के माध्यम से मौन काल में संकेत सूत्रों के रूप में उपलब्ध हुए, उन्हें ही एक क्रम से संग्रहीत करने का प्रयास हुआ है।`,
+        introAuthor: '— श्री अवध किशोर दास',
+    },
+    'dhyan_vallari': {
+        pageCount: 27,
+        title: 'ध्यान वल्लरी',
+        intro: `अपने ध्यान के महासागर की कतिपय सीकारांश का वर्णन पूज्य आचार्यचरण ने अपने इस ग्रंथ श्री ध्यान-वल्लरी में दिग्दर्शित कराया है। जिसका पठन, पाठन... कर हम सभी... युगल सरकार श्री सीतारामजी के प्रेम को प्राप्त कर...`,
+        introAuthor: '— श्री हरिदास जी महाराज<br>अध्यक्ष',
+    },
+    'siddhi_swarup_vaibhav': {
+        pageCount: 459,
+        title: 'सिद्धि स्वरूप वैभव',
+        intro: ``,
+        introAuthor: '',
+    },
+    'atma_vishleshan': {
+        pageCount: 27,
+        title: 'आत्म विश्लेषण',
+        miniBook: 1,
+        intro: ``,
+        introAuthor: '',
+    },
+    'shri_harshan_chalisa': {
+        pageCount: 16,
+        title: 'श्री हर्षण चालीसा',
+        miniBook: 1,
+        intro: ``,
+        introAuthor: '',
+    },
+    'prapatti_prabha_stotram': {
+        pageCount: 64,
+        title: 'प्रपत्ति प्रभा स्तोत्रम्',
+        miniBook: 1,
+        intro: ``,
+        introAuthor: '',
+    },
+    'matru_smriti': {
+        pageCount: 14,
+        title: 'मातृ स्मृति',
+        miniBook: 1,
+        intro: ``,
+        introAuthor: '',
+    },
+};
+
+function bookTemplate(folderName) {
+    var title = booksList[folderName]['title'];
+    var intro = booksList[folderName]['intro'];
+    var introAuthor = booksList[folderName]['introAuthor'];
+    var pageCount = booksList[folderName]['pageCount'];
+    var miniBook = booksList[folderName]['miniBook'] || 0;
+    var bookSize = '';
+    var isMini = miniBook == 1 ? ' small' : '';
+    if (pageCount > 300) {
+        bookSize = ' large';
+    } else if (pageCount > 90) {
+        bookSize = ' medium';
+    }
+    
+    return `<div class="a-box" onmouseenter="bookActive(this)" onmouseleave="bookInactive(this)">
+    <div class="book${isMini}">
+        <div class="book__wrapper${bookSize}"></div>
+        <img class="book__cover" alt="${title}" src="books/${folderName}/${folderName}_1.jpg" />
+    </div>
+    <div class="about-book">
+        <p class="book-title">${title}</p>
+        <div class="book-intro">
+            ${intro}
+            <div class="book-intro-author"><span class="page-count">(कुल पृष्ठ : ${pageCount})</span>${introAuthor}</div>
+        </div>
+        <div class="button-panel">
+            <a class="button" onclick="loadPdf('${folderName}');">पुस्तक पढ़ें</a>
+            <a class="button">डाउनलोड करें</a>
+        </div>
+    </div>
+</div>`;
+}
