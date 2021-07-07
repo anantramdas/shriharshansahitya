123dd8343bdaf63e5df6969ade6bf6a8223dca47
Anantram Das <anantramdas20@gmail.com>
2021-07-07 13:35:39 +0530

    nirnay patra

---
+++ js/nirnay_chart.js
@@ -0,0 +1,146 @@
+var finalDates = {
+	'कामदा': {
+		type: 'ekadashi',
+		date: ['Apr 23, 2021'],
+	},
+	'बरुथिनी': {
+		type: 'ekadashi',
+		date: ['May 7, 2021'],
+	},
+	'मोहिनी': {
+		type: 'ekadashi',
+		date: ['May 23, 2021'],
+	},
+	'अचला': {
+		type: 'ekadashi',
+		date: ['Jun 6, 2021'],
+	},
+	'निर्जला': {
+		type: 'ekadashi',
+		date: ['Jun 21, 2021'],
+	},
+	'योगिनी': {
+		type: 'ekadashi',
+		date: ['Jul 5, 2021'],
+	},
+	'हरिशयनी': {
+		type: 'ekadashi',
+		date: ['Jul 20, 2021'],
+	},
+	'कामिका': {
+		type: 'ekadashi',
+		date: ['Aug 4, 2021'],
+	},
+	'पुत्रदा': {
+		type: 'ekadashi',
+		date: ['Aug 18, 2021'],
+	},
+	'अजा': {
+		type: 'ekadashi',
+		date: ['Sep 3, 2021'],
+	},
+	'पद्मा': {
+		type: 'ekadashi',
+		date: ['Sep 17, 2021'],
+	},
+	'इन्दिरा': {
+		type: 'ekadashi',
+		date: ['Oct 2, 2021'],
+	},
+	'पापंकुशा': {
+		type: 'ekadashi',
+		date: ['Oct 16, 2021'],
+	},
+	'रम्भा': {
+		type: 'ekadashi',
+		date: ['Nov 1, 2021'],
+	},
+	'प्रबोधिनी': {
+		type: 'ekadashi',
+		date: ['Nov 15, 2021'],
+	},
+	'उत्पन्ना': {
+		type: 'ekadashi',
+		date: ['Nov 30, 2021'],
+	},
+	'मोक्षदा': {
+		type: 'ekadashi',
+		date: ['Dec 14, 2021'],
+	},
+	'सफला': {
+		type: 'ekadashi',
+		date: ['Dec 30, 2021'],
+	},
+	'पुत्रदा': {
+		type: 'ekadashi',
+		date: ['Jan 13, 2022'],
+	},
+	'षट्तिला': {
+		type: 'ekadashi',
+		date: ['Jan 28, 2022'],
+	},
+	'जया': {
+		type: 'ekadashi',
+		date: ['Feb 12, 2022'],
+	},
+	'विजया': {
+		type: 'ekadashi',
+		date: ['Feb 27, 2022'],
+	},
+	'आमलकी': {
+		type: 'ekadashi',
+		date: ['Mar 14, 2022'],
+	},
+	'पापमोचनी': {
+		type: 'ekadashi',
+		date: ['Mar 28, 2022'],
+	},
+	'पद्मिनी': {
+		type: 'ekadashi',
+		date: [],
+	},
+	'परमा': {
+		type: 'ekadashi',
+		date: [],
+	},
+	'श्री राम नवमी': {
+		type: 'utsav',
+		date: ['Apr 21, 2021'],
+	},
+	'श्री जानकी नवमी': {
+		type: 'utsav',
+		date: ['May 21, 2021'],
+	},
+	'श्री नृसिंह जयंती': {
+		type: 'utsav',
+		date: ['May 25, 2021'],
+	},
+	'श्रीसरयूजी की जयंती': {
+		type: 'utsav',
+		date: ['Jun 24, 2021'],
+	},
+	'रथ यात्रा': {
+		type: 'utsav',
+		date: ['Jul 12, 2021'],
+	},
+	'श्री गुरु पूर्णिमा': {
+		type: 'utsav',
+		date: ['Jul 24, 2021'],
+	},
+	'गो. तुलसीदास की जयंती': {
+		type: 'utsav',
+		date: ['Aug 15, 2021'],
+	},
+	'रक्षा बंधन': {
+		type: 'utsav',
+		date: ['Aug 22, 2021'],
+	},
+	'श्री कृष्ण जन्माष्टमी': {
+		type: 'utsav',
+		date: ['Aug 31, 2021'],
+	},
+	'श्री प्रेम रामायण जयंती': {
+		type: 'utsav',
+		date: ['Sep 14, 2021'],
+	},
+}
